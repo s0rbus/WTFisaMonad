fe43a50d0fe8e62d7957900d2b8a76d50ef2f107
s0rbus <s0rbus@users.noreply.github.com>
2014-11-21 17:40:10 +0000

    added lifted version of safe-fragile

--- src/no-nil-m.clj
+++ src/no-nil-m.clj
@@ -1,4 +1,4 @@
-(use 'clojure.contrib.monads)
+(use 'clojure.algo.monads)
 
 (defmonad no-nil-m
   [m-result (fn [mv] mv)
@@ -14,11 +14,21 @@
 (fragile 1 2 3)
 (fragile 1 nil 2)
 
-(def safe-fragile (with-monad no-nil-m (m-lift 3 fragile)))
+(defn safe-fragile [a b c]
+  (domonad no-nil-m [safe-a a
+                     safe-b b
+                     safe-c c]
+           (fragile safe-a safe-b safe-c)))
 
 (println "safe--------------------")
 (safe-fragile 1 2 3)
 (safe-fragile 1 nil 2)
+
+(def lifted-safe-fragile (with-monad no-nil-m (m-lift 3 fragile)))
+
+(println "lifted-safe--------------------")
+(lifted-safe-fragile 1 2 3)
+(lifted-safe-fragile 1 nil 2)
 
 (println "done--------------------")
 
