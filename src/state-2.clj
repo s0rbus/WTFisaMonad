8177634af6ddad4e01f57b4243dbbdd8cb0dc19d
s0rbus <soorbus@gmail.com>
2014-11-22 08:44:07 +0000

    Completed transcript Updated state-2 according to talk

--- src/state-2.clj
+++ src/state-2.clj
@@ -1,6 +1,8 @@
-(use 'clojure.contrib.monads)
+(use 'clojure.algo.monads)
 
 (defn get-s [k] (fn [s] [(s k) s]))
+
+(defn print_ [k] (fn [s] [(println k (s k)) s]))
 
 (defn set-s [k v] (fn [s]
   (let [old (s k)]
@@ -15,3 +17,16 @@
 
 (println
   ((sum-s :a :b :t) {:a 1 :b 2}))
+
+(defn run-program []
+   (domonad state-m
+      [
+         _ (set-s :t 1)
+         _ (print_ :t)
+         t (get-s :t)
+         _ (set-s :t (inc t))
+         _ (print_ :t)
+         ]
+    nil))
+
+
